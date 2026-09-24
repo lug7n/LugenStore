2c6a721d8167915a6257bab0b82468efac2a06a0
Luiz Fonseca <lgfonseca3473@gmail.com>
2026-09-24 11:20:56 -0300

    apply migrations add

--- LugenStore.API/Program.cs
+++ LugenStore.API/Program.cs
@@ -1,6 +1,7 @@
 using LugenStore.API.Extensions;
 using LugenStore.Application;
 using LugenStore.Infrastructure;
+using LugenStore.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.ApplyMigrations();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
